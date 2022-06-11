615e8e8eaaab92242eb87720ee1cc3bec5081959
Didi518 <94155950+Didi518@users.noreply.github.com>
2022-06-11 19:38:21 +0200

    react context ajouter dans le panier

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,17 +1,32 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useContext } from 'react';
 import Layout from '../../components/Layout';
 import data from '../../utils/data';
+import { Store } from '../../utils/Store';
 
 export default function ProductScreen() {
+  const { state, dispatch } = useContext(Store);
   const { query } = useRouter();
   const { slug } = query;
   const product = data.products.find((x) => x.slug === slug);
   if (!product) {
     return <div>Article introuvable</div>;
   }
+
+  const addToCartHandler = () => {
+    const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+
+    if (product.countInStock < quantity) {
+      alert("Désolés. Cet article n'est plus disponible.");
+      return;
+    }
+
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+  };
+
   return (
     <Layout title={product.name}>
       <div className="py-2">
@@ -52,7 +67,12 @@ export default function ProductScreen() {
                 {product.countInStock > 0 ? 'En stock' : 'Indisponible'}
               </div>
             </div>
-            <button className="primary-button w-full">Ajouter au panier</button>
+            <button
+              className="primary-button w-full"
+              onClick={addToCartHandler}
+            >
+              Ajouter au panier
+            </button>
           </div>
         </div>
       </div>
