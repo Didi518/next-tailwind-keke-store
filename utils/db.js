274b16ed8fc9439b47417e9766b66aa8e841cec2
Didi518 <94155950+Didi518@users.noreply.github.com>
2022-06-12 10:36:00 +0200

    connecter mongodb

---
+++ utils/db.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const connexion = {};
+
+async function connect() {
+  if (connexion.isConnected) {
+    console.log('déjà connecté');
+    return;
+  }
+  if (mongoose.connections.length > 0) {
+    connexion.isConnected = mongoose.connections[0].readyState;
+    if (connexion.isConnected === 1) {
+      console.log('utiliser la connexion précédente');
+      return;
+    }
+    await mongoose.disconnect();
+  }
+  const db = await mongoose.connect(process.env.MONGODB_URI);
+  console.log('nouvelle connexion');
+  connexion.isConnected = db.connections[0].readyState;
+}
+
+async function disconnect() {
+  if (connexion.isConnected) {
+    if (process.env.NODE_ENV === 'production') {
+      await mongoose.disconnect();
+      connexion.isConnected = false;
+    } else {
+      console.log('non déconnecté');
+    }
+  }
+}
+
+const db = { connect, disconnect };
+export default db;
