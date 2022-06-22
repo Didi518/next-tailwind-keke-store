9ff63bbdbd2a1b227296df1395f1d2036f21d2db
Didi518 <94155950+Didi518@users.noreply.github.com>
2022-06-23 01:02:59 +0200

    page commande

---
+++ pages/commande/[id].js
@@ -0,0 +1,170 @@
+import axios from 'axios';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect, useReducer } from 'react';
+import Layout from '../../components/Layout';
+import { getError } from '../../utils/error';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true, error: '' };
+    case 'FETCH_SUCCESS':
+      return { ...state, loading: false, order: action.payload, error: '' };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      state;
+  }
+}
+function OrderScreen() {
+  // commande/:id
+  const { query } = useRouter();
+  const orderId = query.id;
+
+  const [{ loading, error, order }, dispatch] = useReducer(reducer, {
+    loading: true,
+    order: {},
+    error: '',
+  });
+  useEffect(() => {
+    const fetchOrder = async () => {
+      try {
+        dispatch({ type: 'FETCH_REQUEST' });
+        const { data } = await axios.get(`/api/commandes/${orderId}`);
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+      }
+    };
+    if (!order._id || (order._id && order._id !== orderId)) {
+      fetchOrder();
+    }
+  }, [order, orderId]);
+  const {
+    shippingAddress,
+    paymentMethod,
+    orderItems,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    isPaid,
+    paidAt,
+    isDelivered,
+    deliveredAt,
+  } = order;
+
+  return (
+    <Layout title={`Commande ${orderId}`}>
+      <h1 className="mb-4 text-xl">{`Commande ${orderId}`}</h1>
+      {loading ? (
+        <div>Chargement...</div>
+      ) : error ? (
+        <div className="alert-error">{error}</div>
+      ) : (
+        <div className="grid md:grid-cols-4 md:gap-5">
+          <div className="overflow-x-auto md:col-span-3">
+            <div className="card  p-5">
+              <h2 className="mb-2 text-lg">Adresse de Livraison</h2>
+              <div>
+                {shippingAddress.fullName}, {shippingAddress.address},{' '}
+                {shippingAddress.postalCode}, {shippingAddress.city},{' '}
+                {shippingAddress.country}
+              </div>
+              {isDelivered ? (
+                <div className="alert-success">Livrée le {deliveredAt}</div>
+              ) : (
+                <div className="alert-error">Non livrée</div>
+              )}
+            </div>
+
+            <div className="card p-5">
+              <h2 className="mb-2 text-lg">Mode de Paiement</h2>
+              <div>{paymentMethod}</div>
+              {isPaid ? (
+                <div className="alert-success">Réglée le {paidAt}</div>
+              ) : (
+                <div className="alert-error">Non Réglée</div>
+              )}
+            </div>
+
+            <div className="card overflow-x-auto p-5">
+              <h2 className="mb-2 text-lg">Articles Commandés</h2>
+              <table className="min-w-full">
+                <thead className="border-b">
+                  <tr>
+                    <th className="px-5 text-left">Article</th>
+                    <th className="    p-5 text-right">Quantité</th>
+                    <th className="  p-5 text-right">Prix</th>
+                    <th className="p-5 text-right">Sous-Total</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {orderItems.map((item) => (
+                    <tr key={item._id} className="border-b">
+                      <td>
+                        <Link href={`/article/${item.slug}`}>
+                          <a className="flex items-center">
+                            <Image
+                              src={item.image}
+                              alt={item.name}
+                              width={50}
+                              height={50}
+                            ></Image>
+                            &nbsp;
+                            {item.name}
+                          </a>
+                        </Link>
+                      </td>
+                      <td className=" p-5 text-right">{item.quantity}</td>
+                      <td className="p-5 text-right">{item.price}€</td>
+                      <td className="p-5 text-right">
+                        {item.quantity * item.price}€
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div>
+            <div className="card  p-5">
+              <h2 className="mb-2 text-lg">Résumé de la Commande</h2>
+              <ul>
+                <li>
+                  <div className="mb-2 flex justify-between">
+                    <div>Articles</div>
+                    <div>{itemsPrice}€</div>
+                  </div>
+                </li>{' '}
+                <li>
+                  <div className="mb-2 flex justify-between">
+                    <div>Taxes</div>
+                    <div>{taxPrice}€</div>
+                  </div>
+                </li>
+                <li>
+                  <div className="mb-2 flex justify-between">
+                    <div>Livraison</div>
+                    <div>{shippingPrice}€</div>
+                  </div>
+                </li>
+                <li>
+                  <div className="mb-2 flex justify-between">
+                    <div>Total</div>
+                    <div>{totalPrice}€</div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      )}
+    </Layout>
+  );
+}
+
+OrderScreen.auth = true;
+export default OrderScreen;
